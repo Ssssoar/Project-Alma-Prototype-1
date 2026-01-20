bc2296bda4ccec3c1cf8f694453b891add45c9c5
Ssssoar <jano20@vtr.net>
2026-01-20 17:00:41 -0300

    Organizing Folders, separating generic "Grid Entity" Functionality

---
+++ scripts/grid_entity.gd
@@ -0,0 +1,31 @@
+class_name GridEntity extends Node2D
+
+@export var floor_tile_map: TileMapLayer
+var current_grid_pos: Vector2i 
+
+func _ready() -> void:
+	current_grid_pos = set_current_grid_pos_from_transform()
+
+func set_current_grid_pos_from_transform() -> Vector2i:
+	var local_pos = floor_tile_map.to_local(global_position)
+	return floor_tile_map.local_to_map(local_pos)
+
+func can_move_in_vector(vector: Vector2i) -> bool:
+	var target_grid_pos = current_grid_pos + vector
+	return can_move_to_tile(target_grid_pos)
+
+func can_move_to_tile(tile_pos: Vector2i) -> bool:
+	var target_tile_data = floor_tile_map.get_cell_tile_data(tile_pos)
+	if target_tile_data == null: return false
+	return target_tile_data.get_custom_data("passable")
+
+func move_in_direction(direction: Vector2i, ignore_impassable_terrain: bool = false):
+	var target_position = current_grid_pos + direction
+	move_to_pos(target_position, ignore_impassable_terrain)
+
+func move_to_pos(target_grid_position: Vector2i, ignore_impassable_terrain: bool = false):
+	if (!ignore_impassable_terrain) && (!can_move_to_tile(target_grid_position)):
+		return
+	current_grid_pos = target_grid_position
+	var position_as_local_to_map = floor_tile_map.map_to_local(current_grid_pos)
+	global_position = floor_tile_map.to_global(position_as_local_to_map)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,29 +1,19 @@
-class_name Player extends Node2D
+class_name Player extends GridEntity
 
-@export var floor_tile_map: TileMapLayer
 @export var sprite_node: Sprite2D
 @export var up_sprite_num: int
 @export var down_sprite_num: int
 @export var left_sprite_num: int
 @export var right_sprite_num: int
-var current_grid_pos: Vector2i 
-
-func _ready() -> void:
-	current_grid_pos = get_current_grid_position()
 
 func _process(_delta: float) -> void:
 	movement()
-
-func get_current_grid_position() -> Vector2i:
-	var local_pos = floor_tile_map.to_local(global_position)
-	return floor_tile_map.local_to_map(local_pos)
 
 func movement():
 	var movement_vector = process_input()
 	if movement_vector == Vector2i.ZERO: return
 	set_sprite(movement_vector)
-	if can_move_in_direction(movement_vector):
-		move_in_direction(movement_vector)
+	move_in_direction(movement_vector)
 
 func process_input() -> Vector2i:
 	if Input.is_action_just_pressed("UP"):
@@ -46,14 +36,3 @@ func set_sprite(direction: Vector2i):
 			sprite_node.frame = left_sprite_num
 		Vector2i.RIGHT:
 			sprite_node.frame = right_sprite_num
-
-func can_move_in_direction(direction: Vector2i) -> bool:
-	var target_grid_pos = current_grid_pos + direction
-	var target_tile_data = floor_tile_map.get_cell_tile_data(target_grid_pos)
-	if target_tile_data == null: return false
-	return target_tile_data.get_custom_data("passable")
-
-func move_in_direction(direction: Vector2i):
-	current_grid_pos = current_grid_pos + direction
-	var position_as_local_to_map = floor_tile_map.map_to_local(current_grid_pos)
-	global_position = floor_tile_map.to_global(position_as_local_to_map)
