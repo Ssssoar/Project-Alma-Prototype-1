f9ac6f51a1299e03d5b664ed0c141944ea1b448f
Ssssoar <jano20@vtr.net>
2026-01-29 19:07:59 -0300

    basic health system created

--- scripts/grid_entities/health_system.gd
+++ scripts/grid_entities/health_system.gd
@@ -1,6 +1,7 @@
-class_name Health extends Node
+class_name Health extends Control
 
 @export var starting_health: float
+@export var bar: ProgressBar
 var max_health: float
 var current_health: float
 
@@ -9,13 +10,21 @@ signal health_depleted()
 func _ready() -> void:
 	current_health = starting_health
 	max_health = starting_health
+	update_bar()
 
 func hurt(ammount_lost: float):
 	current_health -= ammount_lost
 	if current_health <= 0.0:
 		health_depleted.emit()
+	update_bar()
 
 func heal(ammount_healed: float):
 	current_health += ammount_healed
 	if current_health > max_health:
 		current_health = max_health
+	update_bar()
+
+func update_bar():
+	bar.max_value = max_health
+	bar.min_value = 0
+	bar.value = current_health
